65096ea2025e202f103e6b1c6fbd2a4bbc49c935
devFormation <metalleafmusic@gmail.com>
2023-10-02 17:55:58 +0300

    added some scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _forseMagnitude;
     [SerializeField] private float _maxVelocity;
+    [SerializeField] private float _speedRotation;
 
 
     private Vector3 _movementDirection;
@@ -29,6 +30,41 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Update()
+    {
+        ProcessInput();
+
+        KeepPlayerOnScreen();
+
+        RotateToFaceVelocity();
+    }
+
+    private void RotateToFaceVelocity()
+    {
+        if (_rb.velocity == Vector3.zero) return;
+        
+        Quaternion targetRotation = Quaternion.LookRotation(_rb.velocity, Vector3.back);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, _speedRotation * Time.deltaTime);
+
+
+    }
+    
+    private void KeepPlayerOnScreen()
+    {
+        Vector3 newPosition = transform.position;
+
+        Vector3 viewPortPosition =_mainCamera.WorldToViewportPoint(transform.position);
+
+        if (viewPortPosition.x > 1) newPosition.x = -newPosition.x + 0.1f;
+        if (viewPortPosition.y > 1) newPosition.y = -newPosition.y + 0.1f;
+        if (viewPortPosition.x < 0) newPosition.x = -newPosition.x - 0.1f;
+        if (viewPortPosition.y < 0) newPosition.y = -newPosition.y - 0.1f;
+
+        transform.position = newPosition;
+    }
+
+
+    private void ProcessInput()
     {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
