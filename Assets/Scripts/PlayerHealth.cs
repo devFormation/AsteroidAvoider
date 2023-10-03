1e3f5f464b531b2d04adbfccc5a306a18290cd62
devFormation <metalleafmusic@gmail.com>
2023-10-03 11:23:13 +0300

    Added main menu script

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     public void Crush()
     {
         gameObject.SetActive(false);
+        SceneManager.LoadScene(0);
     }
 }
