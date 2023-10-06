0f5eae4746c09b8e2232ca01ebc52e2bb41557f7
devFormation <metalleafmusic@gmail.com>
2023-10-06 17:02:04 +0300

    score system implemented

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,10 +9,17 @@ public class GameOverHandler : MonoBehaviour
 {
     [SerializeField] private GameObject _gameOverDisplay;
     [SerializeField] private AsteroidSpawner _asteroidSpawner;
+    [SerializeField] private TMP_Text _gameOverText;
+    [SerializeField] private ScoreSystem _scoreSystem;
 
     public void EndGame()
     {
         _asteroidSpawner.enabled = false;
+
+        int _finalScore = _scoreSystem.EndTimer();
+
+        _gameOverText.text = $"Your score is: {_finalScore}";
+        
         _gameOverDisplay.gameObject.SetActive(true);
     }
 
